1d699b3b9d44b3973188be9ceab331c091387b51
nghiahuynh-ai <huynhnguyenhieunghia1999@gmail.com>
2023-08-01 14:07:54 +0700

    upload project

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+with open('requirements.txt') as fin:
+    lines = fin.readlines()
+    lines = [o.strip() for o in lines]
+    lines = [o for o in lines if len(o) > 0]
+    req = [o for o in lines if not o.startswith('#') and not o.startswith('git+')]
+
 setup(
     name = "unet",
     version = "0.1",
