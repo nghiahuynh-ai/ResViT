7378d4b46577587ea62471d9e463a9e0d62b83b0
nghiahuynh-ai <huynhnguyenhieunghia1999@gmail.com>
2023-08-22 16:08:38 +0700

    update

--- resvit/utils/gen_label.py
+++ resvit/utils/gen_label.py
@@ -3,12 +3,30 @@ import cv2
 from shapely.geometry import Polygon
 import pyclipper
 
-def gen_label(image, polygons):
+def gen_label(image, polygons, min_text_size=8, shrink_ratio=0.4):
         
     h, w, _ = image.shape
-
     gt = np.zeros((h, w), dtype=np.float32)
-    cv2.fillPoly(gt, polygons, 1)
+    
+    for i in range(len(polygons)):
+        polygon = polygons[i]
+        height = max(polygon[:, 1]) - min(polygon[:, 1])
+        width = max(polygon[:, 0]) - min(polygon[:, 0])
+            
+        if min(height, width) < min_text_size:
+            cv2.fillPoly(gt, polygons, 1)
+        else:
+            polygon_shape = Polygon(polygon)
+            distance = polygon_shape.area * (1 - np.power(shrink_ratio, 2)) / polygon_shape.length
+            subject = [tuple(l) for l in polygons[i]]
+            padding = pyclipper.PyclipperOffset()
+            padding.AddPath(subject, pyclipper.JT_ROUND, pyclipper.ET_CLOSEDPOLYGON)
+            shrinked = padding.Execute(-distance)
+            if shrinked == []:
+                cv2.fillPoly(gt, polygon.astype(np.int32), 1)
+                continue
+            shrinked = np.array(shrinked[0]).reshape(-1, 2)
+            cv2.fillPoly(gt, [shrinked.astype(np.int32)], 1)
         
     return gt
 
